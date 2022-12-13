47cc8dfe5dc68249d47c68d44436fdf5064fcebe
Sephiroth <78663409+sephirothcoding@users.noreply.github.com>
2022-12-13 13:43:11 +0200

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,13 @@
+// Used jquery v3.5.1 and font-awesome v4.7.0
+$(document).ready(function() {
+    $('.nav .dropdown-menu').prev('a').on('click', function(e) {
+      e.preventDefault();
+      $(this).parent().find('.dropdown-menu').slideToggle();
+    });
+});
+
+
+
 // Increments the delay on each item.
 $('.rolldown-list li').each(function () {
   var delay = ($(this).index() / 4) + 's';
