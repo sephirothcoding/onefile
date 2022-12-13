737b38d99fd3f7ed97562cdef5685b9f6c496e64
Sephiroth <78663409+sephirothcoding@users.noreply.github.com>
2022-12-13 13:44:02 +0200

    Update vectro.js

--- vectro.js
+++ vectro.js
@@ -1,5 +1,1 @@
-if ( navigator.platform.indexOf('Win') != -1 ) {
-  window.document.getElementById("wrapper").setAttribute("class", "windows");
-} else if ( navigator.platform.indexOf('Mac') != -1 ) {
-  window.document.getElementById("wrapper").setAttribute("class", "mac");
-}
+
